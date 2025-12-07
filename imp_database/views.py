59161b6e84810381dd860729cfb6c9b182dd8327
Ho1ocron <cmrfreeze@gmail.com>
2025-12-07 13:35:30 +0500

    style update

--- imp_database/views.py
+++ imp_database/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
+
 def render_index(request: HttpRequest) -> HttpResponse:
     return render(request, "index.html")
